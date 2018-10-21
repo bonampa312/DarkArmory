d8486ef0a2665b504434289a384d84151a4c2a59
Santiago Romero <santiago.romero@globant.com>
2018-10-21 18:21:48 -0500

    DA_Networking_03 added networking management for elements details

---
+++ DarkArmory/Core/UseCases/RequestElementsDetail.swift
@@ -0,0 +1,9 @@
+//
+//  RequestElementsDetail.swift
+//  DarkArmory
+//
+//  Created by Santiago Romero Restrepo on 10/21/18.
+//  Copyright © 2018 Santiago Romero Restrepo. All rights reserved.
+//
+
+import Foundation
